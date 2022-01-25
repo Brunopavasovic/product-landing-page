4ebb1de30d537c68f98f5a40b53ba9dc9d3765c6
Bruno Pavasović <brunopavasovic@gmail.com>
2022-01-25 13:44:47 +0100

    Make site responsive, reformat everything

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const hamburger = document.getElementById("menu-btn");
 const listWrapper = document.querySelector(".list-wrapper");
-const closeMenu = document.getElementById('close');
+const closeMenu = document.getElementById("close");
 
-hamburger.addEventListener('click', () => {
-    listWrapper.classList.toggle('show');
-  
-}
-)
+hamburger.addEventListener("click", () => {
+  listWrapper.classList.toggle("show");
+});
 
-closeMenu.addEventListener('click' , () => {
-    listWrapper.classList.remove('show');
-})
+closeMenu.addEventListener("click", () => {
+  listWrapper.classList.remove("show");
+});
